0b570762bb981e1ab0f83a21e7f8244521c4df14
Brando <branmuller@gmail.com>
2020-12-30 07:55:34 -0600

    Filled out the skeleton for card swiping Added a item.xml file for the cards imported a card swiping library by adding gradle dependency

--- app/src/main/java/com/example/pitchit/MainActivity.kt
+++ app/src/main/java/com/example/pitchit/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.pitchit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.lorentzos.flingswipe.SwipeFlingAdapterView
 
 class MainActivity : AppCompatActivity() {
+
+    private var al = ArrayList<String>()
+    private var arrayAdapter: ArrayAdapter<String>? = null
+    private var i = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var frame = findViewById<SwipeFlingAdapterView>(R.id.frame)
+
+        al.add("php")
+        al.add("c")
+        al.add("python")
+        al.add("java")
+
+        arrayAdapter = ArrayAdapter<String>(this, R.layout.item, R.id.helloText, al )
+
+        //set the listener and the adapter
+        frame.adapter = arrayAdapter
+        frame.setFlingListener(object: SwipeFlingAdapterView.onFlingListener {
+            override fun removeFirstObjectInAdapter() {
+                Log.d("LIST", "removed object!")
+                al.removeAt(0)
+                arrayAdapter?.notifyDataSetChanged()
+            }
+
+            override fun onLeftCardExit(p0: Any?) {
+                Toast.makeText(this@MainActivity, "Left!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onRightCardExit(p0: Any?) {
+                Toast.makeText(this@MainActivity, "Right!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAdapterAboutToEmpty(p0: Int) {
+                al.add("XML $i")
+                arrayAdapter?.notifyDataSetChanged()
+                Log.d("LIST", "notified")
+                i++
+            }
+
+            override fun onScroll(p0: Float) {
+            }
+
+        })
     }
 }
